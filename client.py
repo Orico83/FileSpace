8f86dd16796a500d29c5e9e4a42e9a446e19a3f4
Orico83 <113985668+Orico83@users.noreply.github.com>
2023-03-09 12:37:48 +0200

    Update client.py

--- client.py
+++ client.py
@@ -14,38 +14,28 @@ win = Tk()
 win.geometry("750x250")
 
 
-def get_user():
-    global entry, username
-    username = entry.get()
+def login():
+    global username, password
+    username = username_entry.get()
+    password = password_entry.get()
+    print(username)
+    print(password)
+    client_socket.send(dumps([username, password]))
 
 
-def get_password():
-    global entry, password
-    password = entry.get()
+username_label = Label(win, text="Username:")
+username_label.pack()
+username_entry = Entry(win)
+username_entry.pack()
 
+password_label = Label(win, text="Password:")
+password_label.pack()
+password_entry = Entry(win, show="*")
+password_entry.pack()
 
-# Initialize a Label to display the User Input
-while username == '':
-    label = Label(win, text="Enter Username", font="Courier 22 bold")
-    label.pack()
-    # Create an Entry widget to accept User Input
-    entry = Entry(win, width=40)
-    entry.focus_set()
-    entry.pack()
-    ttk.Button(win, text="Okay", width=20, command=get_user()).pack(pady=20)
-    win.mainloop()
-
-
-label = Label(win, text="Enter Password", font="Courier 22 bold")
-label.pack()
-entry = Entry(win, width=40)
-entry.focus_set()
-entry.pack()
-ttk.Button(win, text="Okay", width=20, command=get_password).pack(pady=20)
+button = Button(win, text="Log In", command=login)
+button.pack()
 
 win.mainloop()
 
-client_socket.send(dumps([username, password]))
 
-message = client_socket.recv(1024).decode()
-print(message)
